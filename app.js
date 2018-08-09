c8237886d8f05d5e81427ed4e22fc8b1c2b65dd8
Germán Escobar <german.escobarc@gmail.com>
2018-08-09 10:39:01 -0400

    Initial import

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+
+const VisitorSchema = new mongoose.Schema({
+  name: {
+    type: String
+  }
+});
+const Visitor = mongoose.model("Visitor", VisitorSchema);
+
+app.get("/", async (req, res) => {
+  const visitor = new Visitor({ name: "German" });
+  await visitor.save()
+
+  res.send("<h1>El visitante fue almacenado con éxito.</h1>")
+});
+
+app.listen(3000);
